6fefb778ba405cb8f154a84abac22545897f08c8
fasola23 <158285427+fasola23@users.noreply.github.com>
2024-02-06 08:28:06 +0000

    ok

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/snakeGame', { useNewUrlParser: true
