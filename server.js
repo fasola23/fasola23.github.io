d61ff7dd4cc9411d1f84deec6d5a97a574ac1ccc
fasola23 <158285427+fasola23@users.noreply.github.com>
2024-02-06 08:30:23 +0000

    ok

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/snakeGame', { useNewUrlParser: true
+mongoose.connect('mongodb://localhost:27017/snakeGame', { useNewUrlParser: true})
