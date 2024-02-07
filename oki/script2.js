69c5f9a1f08c450634bc86f093174a3cca130ca4
fasola23 <158285427+fasola23@users.noreply.github.com>
2024-02-07 09:35:51 +0000

    ok

--- oki/script2.js
+++ oki/script2.js
@@ -1,56 +1,85 @@
-stałe data = nowa data();
+const date = new Date();
 
 const renderCalendar = () => {
-  data.setData(1);
+  date.setDate(1);
 
-  const miesiącDays = document.querySelector(.'days");
+  const monthDays = document.querySelector(".days");
 
-  const ostatni dzień = nowa data (
-    data.getFullYear(),
-    data.getMonth() + 1,
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
     0
   ).getDate();
 
-  const prevLastDay = nowa data (
-    data.getFullYear(),
-    data.getMonth(),
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
     0
   ).getDate();
 
-  const pierwszyDayIndex = date.getDay();
+  const firstDayIndex = date.getDay();
 
-  const lastDayIndex = nowa data (
-    data.getFullYear(),
-    data.getMonth() + 1,
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
     0
   ).getDay();
 
   const nextDays = 7 - lastDayIndex - 1;
 
-  stałe miesiące = [
-    „Styczeń”,
-    „Luty”,
-    „Marca”,
-    „Kwiecień”,
-    „Maj”,
-    „Czerwiec”,
-    „Lipiec”,
-    „Sierpień”,
-    „Wrzesień”,
-    „Październik”,
-    „Listopa”,
-    „Grudzień”,
+  const months = [
+    "Styczeń",
+    "Luty",
+    "Marzec",
+    "Kwiecień",
+    "Maj",
+    "Czerwiec",
+    "Lipiec",
+    "Sierpień",
+    "Wrzesień",
+    "Październik",
+    "Listopad",
+    "Grudzień",
   ];
 
-  document.querySelector(.data h1").innerHTML = miesiące[data.getMonth()];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-  document.querySelector(.date p").innerHTML = new Date().toDateString();
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
 
-  niech dni = "";
+  let days = "";
 
-  for (niech x = FirstDayIndex; x > 0; x--) {
-    dni += `<div class="prev-date">${prevLastDay - x + 1</div>`;
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
   }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
 
   for (niech i = 1; i <= ostatni dzień; i++) {
     Jeśli (
