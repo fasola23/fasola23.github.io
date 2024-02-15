184729b9962b767a55bef697925bb22c9d057a57
fasola23 <158285427+fasola23@users.noreply.github.com>
2024-02-15 09:48:42 +0000

    ok

--- wizyty.php
+++ wizyty.php
@@ -143,8 +143,39 @@
                         <tr>
                             <td>Wybierz usługę:</td>
                                 <td><salect name="zabieg">
-                                <option values="2"
+                                <option values="2">Manicure tradycyjny</option>
+                                <option values="3">Manicure japoński</option>
+                                <option values="4">Manicure hybrydowy</option>
+                                <option values="5">Pedicure tradycyjny</option>
+                                <option values="1">Pedicure tradycyjny</option>
+                                <option values="6">Zabieg parafinowy na dłonie</option>
+                                     </select>
+                            </td>
                         </tr>
+                        <tr><td colspan="2"><input type="submit" id="przycisk2" value="Zapisz wizytę">
+                        </td></tr></table>
+                    </form>
+                    <?php
+                        $conn=mysqli_connect('localhost','root','','salon');
+                        if (isset($_POST['imie'])&&isset($_POST['nazw'])&&isset($_POST['data'])&&isset($_POST['zabieg']))
+                        {
+                            $imie=$_POST['imie'];
+                            $nazw=$_POST['nazw'];
+                            $data=$_POST['data'];
+                            $zabieg=$_POST['zabieg'];
+                            $q1=mysqli_query($conn,"SELECT Idklienta FROM klient WHERE Imie='$imie' and Nazwisko='$nazw' ");
+                            $r1=mysqli_fetch_array($q1);
+                            $d= $r1['Idklienta'];
+                            $q2=mysqli_query($conn,"SELECT max(Idklienta) as 'ile' FROM klient");
+                                $r2=mysqli_fetch_array($q2);
+                                    if ($r1! =NULL)
+                                    {
+                                        $q3=mysqli_query($conn,"INSERT INTO wizyta (Idklienta,Iduslugi,Datazabiegu) values ('$d','$zabieg','$data')");
+                                            echo "<h4>Wizyta została zapisana.</h4>";
+                                    }
+                                    else {echo "<h4>Nie ma cię w bazie.</h4>";}
+                        }    
+                        ?>           
                 </div>
             </div>
     </div>
